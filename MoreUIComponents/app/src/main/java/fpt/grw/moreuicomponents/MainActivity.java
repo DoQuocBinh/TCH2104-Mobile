5345c19183b2df5c992eaf3bfcc6ebfb978831f7
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-09 12:07:56 +0700

    ok

--- MoreUIComponents/app/src/main/java/fpt/grw/moreuicomponents/MainActivity.java
+++ MoreUIComponents/app/src/main/java/fpt/grw/moreuicomponents/MainActivity.java
@@ -1,10 +1,20 @@
 package fpt.grw.moreuicomponents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,44 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>
                 (this, android.R.layout.simple_list_item_1,countries);
         sp.setAdapter(adapter);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPressed();
+            }
+        });
+
+        BottomNavigationView nav_bottom = findViewById(R.id.bottomNavigationView);
+        nav_bottom.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.music:
+                        Toast.makeText(getApplicationContext(),"Music",Toast.LENGTH_SHORT).show();
+                        openMusicPage();
+                        return true;
+                    case R.id.movie:
+                        Toast.makeText(getApplicationContext(),"Movie",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+    private void openMusicPage() {
+        Intent intent = new Intent(this,Music.class);
+        startActivity(intent);
+    }
+
+    private void buttonPressed() {
+        TextInputLayout textInput = findViewById(R.id.textInputLayout);
+        String name = textInput.getEditText().getText().toString();
+        Toast.makeText(getApplicationContext(),"Hello " + name
+                        ,Toast.LENGTH_LONG).show();
+
+    }
+
     private String[] countries = new String[] {"Vietnam","Singapor","Malaysia"};
 }
