68a3552759fc00afd33d2892d42f5a3590d5b646
DoQuocBinh <binhdq@fpt.edu.vn>
2022-09-06 10:49:27 +0700

    update 2

--- dbIonicDemo/src/pages/Home.tsx
+++ dbIonicDemo/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -6,17 +6,28 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="secondary">
+          <IonTitle>Customer management!</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonItem>
+          <IonLabel>Name</IonLabel>
+          <IonInput></IonInput>
+        </IonItem>
+        <IonItem>
+           <IonLabel>Language</IonLabel>
+           <IonSelect multiple>
+              <IonSelectOption value="The UK">The UK</IonSelectOption>
+              <IonSelectOption value="The USA">The USA</IonSelectOption>
+              <IonSelectOption value="Spain">Spain</IonSelectOption>
+            </IonSelect>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Picture</IonLabel>
+          <IonInput></IonInput>
+        </IonItem>
+        <IonButton expand='block' class='ion-margin'>Save</IonButton>
       </IonContent>
     </IonPage>
   );
