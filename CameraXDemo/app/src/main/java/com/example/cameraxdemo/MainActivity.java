c23691f1caa7ccc48d81cc6f79bc843c012edbad
DoQuocBinh <binhdq@fpt.edu.vn>
2022-10-04 11:48:21 +0700

    gallery

--- CameraXDemo/app/src/main/java/com/example/cameraxdemo/MainActivity.java
+++ CameraXDemo/app/src/main/java/com/example/cameraxdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cameraxdemo;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,14 @@ import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
@@ -24,7 +31,9 @@ import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -62,14 +71,72 @@ public class MainActivity extends AppCompatActivity {
                    Toast.makeText(this, "Take Photo", Toast.LENGTH_SHORT).show();
                    takePhoto();
                }else if(items[item].equals("Choose from Library")){
-                   Toast.makeText(this, "From Library", Toast.LENGTH_SHORT).show();
+                   selectPictureFromGallery();
                }else if(items[item].equals("View picture from an URI")){
-                   Toast.makeText(this, "From URI", Toast.LENGTH_SHORT).show();
+                   String uri = inputPictureUri.getText().toString();
+                   File file = new File(uri);
+                   Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                   imageView.setImageBitmap(bitmap);
+                   dialog.dismiss();
                }
             });
             dlg.show();
         });
     }
+
+    private void selectPictureFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        launchActivity.launch(intent);
+    }
+    ActivityResultLauncher<Intent> launchActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode()== Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    Uri selectedImage = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),selectedImage);
+                        imageView.setImageBitmap(bitmap);
+                        //download the file for gallery to local folder
+                        long timestamp = System.currentTimeMillis();
+                        File file = bitmapToFile(bitmap,
+                                getApplicationContext().getFilesDir().getAbsolutePath() + File.separator
+                        + String.valueOf(timestamp));
+                        inputPictureUri.setText(file.getAbsolutePath());
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+    );
+    public static File bitmapToFile(Bitmap bitmap, String filePath) {
+        //create a file to write bitmap data
+        File file = null;
+        try {
+            file = new File(filePath);
+            file.createNewFile();
+
+            //Convert bitmap to byte array
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0 , bos); // YOU can also save it in JPEG
+            byte[] bitmapdata = bos.toByteArray();
+
+//write the bytes in file
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+            return file;
+        }catch (Exception e){
+            e.printStackTrace();
+            return file; // it will return null
+        }
+    }
+
+
     private void takePhoto() {
         long timestamp = System.currentTimeMillis();
         File savedFile = new File(getApplicationContext().getFilesDir(),String.valueOf(timestamp));
@@ -92,7 +159,6 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                     }
-
                     @Override
                     public void onError(@NonNull ImageCaptureException exception) {
 
